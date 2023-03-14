500ce96df245e1f0b5a6db3bf76b1375ada45442
Deynni Almazan <deynnialmazan@hotmail.com>
2023-03-14 14:32:37 -0500

    Saving project

---
+++ README.md
@@ -0,0 +1,11 @@
+### Alarm Clock
+
+Hello.
+
+This is my alarm clock. 
+
+## DEMO
+
+See DEMO [here](https://deynnialmazan.github.io/alarm-clock/)
+
+![](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
---
+++ assets/script/index.js
@@ -0,0 +1,71 @@
+'use strict';
+const shapeSelect = document.getElementById("shapeSelect");
+const colorSelect = document.getElementById("colorSelect");
+const grid = document.getElementById("grid");
+const createBtn = document.getElementById("createBtn");
+
+const shapes = []; // Array to save shapes (name, color)
+const maxShapes = 20; //Max content for the div container
+
+//Creating class with 2 properties (name, color)
+class Shape {
+    #name;
+    #color;
+  
+    constructor(name, color) {
+        this.#name = name;
+        this.#color = color;
+    }
+  
+    get name() {
+        return this.#name;
+    }
+  
+    get color() {
+        return this.#color;
+    }
+  
+    getInfo() {
+        //Validation to return color name in format like 'Blue, Green...'
+        if (this.#color === "#09f") return `${(this.#name).charAt(0).toUpperCase()+ (this.#name).slice(1)} Blue`;
+        if (this.#color === "#9f0") return `${(this.#name).charAt(0).toUpperCase()+ (this.#name).slice(1)} Green`;
+        if (this.#color === "#f90") return `${(this.#name).charAt(0).toUpperCase()+ (this.#name).slice(1)} Orange`;
+        if (this.#color === "#f09") return `${(this.#name).charAt(0).toUpperCase()+ (this.#name).slice(1)} Pink`;
+        if (this.#color === "#90f") return `${(this.#name).charAt(0).toUpperCase()+ (this.#name).slice(1)} Purple`;
+    }
+  }
+
+  
+  //Function to add divs with the shape/color & to show info
+  function createShape() {
+    const shapeType = shapeSelect.value; // box input
+    const shapeColor = colorSelect.value; // box input
+    const shape = new Shape(shapeType, shapeColor); // Creating object 'shape'
+    
+    if (shapes.length >= maxShapes) {
+        fullStorageMsg.style.visibility = 'visible';
+        return;
+    }
+
+    shapes.push(shape); 
+    const div = document.createElement("div");
+    div.className = `${shapeType} shape`;
+    div.style.backgroundColor = shapeColor;
+    
+    grid.appendChild(div);
+
+
+    const  infoShape = shape.getInfo()
+    const positionShape = shapes.lastIndexOf(shape) + 1 
+    // getting the position of the shape in the array
+    //console.log(positionShape);
+
+    //Event to show info when a div is clicked
+    div.addEventListener("click", () => 
+    infoMsg.innerHTML =  `Unit ${positionShape}: ${infoShape}`);
+  }
+
+ //Creating shapes
+  createBtn.addEventListener("click", () => {
+    createShape();
+  })
